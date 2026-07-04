42d49e604c9655cc580558fb1e6c1dd361ff5146
Leonardo <lm.moreira@gmail.com>
2026-07-03 21:34:43 -0300

    test(web): format coverage config

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -25,7 +25,14 @@ export default defineConfig({
         functions: 90,
         lines: 90,
       },
-      include: ['lib/**', 'app/**', 'components/**', 'providers/**', 'features/**', 'playwright.config.ts'],
+      include: [
+        'lib/**',
+        'app/**',
+        'components/**',
+        'providers/**',
+        'features/**',
+        'playwright.config.ts',
+      ],
       exclude: [
         '**/*.spec.*',
         '**/node_modules/**',
